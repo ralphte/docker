f4ac1aec280444f258998e0daad6cf067b37cae8
ralph <ralphte@mbpro15.local>
2019-08-01 19:27:48 -0400

    Add mailpiler
    New docker image

---
+++ mail-piler/rootfs/tmp/startup.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+#set -x
+
+script_path=$(readlink -f "$0")
+basedir=${script_path%/*}
+script_name=${script_path##*/}
+
+DATAROOTDIR="/usr/share"
+SYSCONFDIR="/usr/local/etc"
+SPHINXCFG="/usr/local/etc/piler/sphinx.conf"
+PILER_HOST=${PILER_HOST:-archive.yourdomain.com}
+PILER_CONF="/usr/local/etc/piler/piler.conf"
+PILER_PEM="/usr/local/etc/piler/piler.pem"
+CONFIG_SITE_PHP="/usr/local/etc/piler/config-site.php"
+WAIT_FOR_IT="/usr/local/etc/piler/wait.sh"
+PILER_MYSQL_CNF="/usr/local/etc/piler/.my.cnf"
+SSL_CERT_DATA="/C=US/ST=Denial/L=Springfield/O=Dis/CN=${PILER_HOSTNAME}"
+
+wait_for_sql() {
+   echo "Waiting for the SQL database to come online"
+   "$WAIT_FOR_IT" "${MYSQL_HOSTNAME}:3306 -s"
+}
+
+update_config_files() {
+   printf "[mysql]\nhost = ${MYSQL_HOSTNAME}\nuser = ${MYSQL_USERNAME}\npassword = ${MYSQL_PASSWORD}\n\n[mysqldump]\nhost = ${MYSQL_HOSTNAME}\nuser = ${MYSQL_USERNAME}\npassword = ${MYSQL_PASSWORD}\n" > "$PILER_MYSQL_CNF"
+   chown piler:piler "$PILER_MYSQL_CNF"
+   chmod 400 "$PILER_MYSQL_CNF"
+
+   echo "Updating sphinx configuration"
+   sed -e "s%MYSQL_HOSTNAME%$MYSQL_HOSTNAME%" -e "s%MYSQL_DATABASE%$MYSQL_DATABASE%" -e "s%MYSQL_USERNAME%$MYSQL_USERNAME%" -e "s%MYSQL_PASSWORD%$MYSQL_PASSWORD%" $SYSCONFDIR/piler/sphinx.conf.dist > $SPHINXCFG
+   echo "Done."
+
+   if [[ ! -f "$PILER_CONF" ]]; then
+
+      pilerconf | grep -v mysqlsocket | \
+      sed -e "s/tls_enable=0/tls_enable=1/g" \
+          -e "s/hostid=mailarchiver/hostid=${PILER_HOSTNAME}/g" \
+          -e "s/mysqlport=0/mysqlport=3306/" \
+          -e "s/mysqlpwd=/mysqlpwd=${MYSQL_PASSWORD}/" \
+          -e "s/mysqlhost=/mysqlhost=${MYSQL_HOSTNAME}/" \
+          -e "s/mysqluser=piler/mysqluser=${MYSQL_USERNAME}/" \
+          -e "s/mysqldb=piler/mysqldb=${MYSQL_DATABASE}/" \
+          -e "s/pemfile=/pemfile=\/etc\/piler\/piler.pem/" > "$PILER_CONF"
+
+      chmod 600 "$PILER_CONF"
+      chown $PILER_USER:$PILER_USER /usr/local/etc/piler/piler.conf
+   fi
+
+   sed -i -e '/\$config\['\''SITE_NAME'\''\]/ s/= .*/= '\'''${PILER_HOSTNAME}''\'';/' "$CONFIG_SITE_PHP"
+   sed -i -e '/\$config\['\''DB_DATABASE'\''\]/ s/= .*/= '\'''${MYSQL_DATABASE}''\'';/' "$CONFIG_SITE_PHP"
+   sed -i -e '/\$config\['\''DB_PASSWORD'\''\]/ s/= .*/= '\'''${MYSQL_PASSWORD}''\'';/' "$CONFIG_SITE_PHP"
+   sed -i -e '/\$config\['\''DB_HOSTNAME'\''\]/ s/= .*/= '\'''${MYSQL_HOSTNAME}''\'';/' "$CONFIG_SITE_PHP"
+   sed -i -e '/\$config\['\''DB_USERNAME'\''\]/ s/= .*/= '\'''${MYSQL_USERNAME}''\'';/' "$CONFIG_SITE_PHP"
+
+   make_certificate
+}
+
+
+make_certificate() {
+   if [[ ! -f "$PILER_PEM" ]]; then
+      echo -n "Making an ssl certificate ... "
+      openssl req -new -newkey rsa:4096 -days 3650 -nodes -x509 -subj "$SSL_CERT_DATA" -keyout "$PILER_PEM" -out 1.cert -sha1
+      cat 1.cert >> "$PILER_PEM"
+      chmod 640 "$PILER_PEM"
+      chgrp "$PILER_USER" "$PILER_PEM"
+      rm 1.cert
+   fi
+}
+
+
+initialize_piler_data() {
+   echo "Initilizing piler data"
+
+   if [[ "$(echo "show tables" | mysql --defaults-file="$PILER_MYSQL_CNF" "$MYSQL_DATABASE")" == "" ]]; then
+      echo "create database if not exists piler character set utf8mb4" | mysql --defaults-file="$PILER_MYSQL_CNF"
+      mysql --defaults-file="$PILER_MYSQL_CNF" "$MYSQL_DATABASE" < /usr/share/piler/db-mysql.sql
+   fi
+
+   echo "Restarting searchd"
+   supervisorctl restart searchd
+   [[ -f /var/piler/sphinx/main1.spp ]] || su $PILER_USER -c "indexer --all --config ${SPHINXCFG}"
+
+}
+
+wait_for_sql
+update_config_files
+initialize_piler_data
